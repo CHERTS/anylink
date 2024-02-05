98f304d69c5947aec27b57e3a047bc20b5a91011
Mikhail Grigorev <sleuthhound@gmail.com>
2024-02-06 00:28:51 +0500

    Fixed deploy

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,11 @@ deploy="anylink-deploy"
 rm -rf $deploy ${deploy}.tar.gz
 mkdir $deploy
 mkdir $deploy/log
+mkdir $deploy/systemd
 cp -r server/anylink $deploy
 cp -r server/bridge-init.sh $deploy
 cp -r server/conf $deploy
-cp -r systemd $deploy
+cp -r deploy/anylink.service $deploy/systemd
 cp -r LICENSE $deploy
 cp -r index_template $deploy
 tar zcvf ${deploy}.tar.gz $deploy
